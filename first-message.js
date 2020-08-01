6197577d72d63f0749572161da49138ab3ba3c54
nikolozinika67 <kaxa487@gmail,com>
2020-08-01 18:50:47 +0400

    NikoBot

--- first-message.js
+++ first-message.js
@@ -4,22 +4,23 @@ const addReactions = (message, reactions) => {
     if (reactions.length > 0) {
       setTimeout(() => addReactions(message, reactions), 750)
     }
-}
-
-module.exports = async (client, id, text, reactions = []) => {
+  }
+  
+  module.exports = async (client, id, text, reactions = []) => {
     const channel = await client.channels.fetch(id)
-
+  
     channel.messages.fetch().then((messages) => {
-        if (messages.size === 0) {
-           channel.send(text).then(message => {
-             addReactions(message, reactions)
-           })
-        } else {
-
-          for (const message of messages) {
-            message[1].edit(text)
-            addReactions(message[1], reactions)
-          }
+      if (messages.size === 0) {
+        // Send a new message
+        channel.send(text).then((message) => {
+          addReactions(message, reactions)
+        })
+      } else {
+        // Edit the existing message
+        for (const message of messages) {
+          message[1].edit(text)
+          addReactions(message[1], reactions)
         }
+      }
     })
-}
+  }

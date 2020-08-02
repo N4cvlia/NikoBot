86354ca8f3bcbdfa465177abf0b4273075a4d43b
nikolozinika67 <kaxa487@gmail,com>
2020-08-02 16:16:48 +0400

    NikoBot

--- index.js
+++ index.js
@@ -71,7 +71,35 @@ client.on('ready', () =>{
     message.channel.send(embed)
   })
   command(client, 'serverinfo', message => {
-    message.channel.send('Coming soon')
+    const { guild } = message
+    console.log(guild)
+
+    const { name, region, memberCount, owner, afkTimeout } = guild
+    const icon = guild.iconURL()
+
+    const embed = new Discord.MessageEmbed()
+      .setTitle('Server info for "${name}"')
+      .setThumbnail(icon)
+      .addFields(
+      {
+        name: 'Region',
+        value: region,
+      },
+      {
+        name: 'Members',
+        value: memberCount,
+      },
+      {
+        name: 'Owner',
+        value: owner.user.tag,
+      },
+      {
+        name: 'AFK Timeout',
+        value: afkTimeout / 60,
+      }
+    )
+
+    message.channel.send(embed)  
   })
 })
 

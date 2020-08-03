84a77794a544e569b2f982acbe2c0f7752db6666
nikolozinika67 <kaxa487@gmail,com>
2020-08-03 14:23:27 +0400

    NikoBot

--- index.js
+++ index.js
@@ -124,14 +124,23 @@ These are my supported commands:
   command(client, 'ban', message => {
     const { member, mentions} = message
 
+    const tag = `<@${member.id}>`
+
     if (
       member.hasPermission('ADMINISTRATOR') || 
       member.hasPermission('BAN_MEMBERS') 
     ) {
-      console.log('Works')
+      const target = mentions.users.first()
+      if (target) {
+        const targetMember = message.guild.members.cache.get(target.id)
+        targetMember.ban()
+        message.channel.send(`${tag} That user has been banned.`)
+      } else {
+        message.channel.send(`<@${tag}> Please specify someone to ban.`)
+      }
     } else {
       message.channel.send(
-        `<@${member.id}> You do not have permission to use this command.`
+        `<@${tag}> You do not have permission to use this command.`
       )
     }    
   })
